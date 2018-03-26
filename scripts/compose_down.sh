ab1c1f35cad301da87ec427c8c45f892a32a4975
Gustavo Aragon <gustavo.aragon@fit.fraunhofer.de>
2018-03-26 10:50:45 +0200

    .sh for finishing docker compose containers

---
+++ scripts/compose_down.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -e
+
+# Build the containers
+docker-compose -f docker-compose.yml down
+
+# Launch the db alone once and give it time to create db user and database
+# This is a quickfix to avoid waiting for database to startup on first execution (more details [here](https://docs.docker.com/compose/startup-order/))
+#docker-compose -f docker/docker-compose.yml up -d db
+#sleep 5
+#docker-compose -f docker/docker-compose.dev.yml stop db
