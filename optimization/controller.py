63999c2fcd130c16240d22ba2f65924c88390430
Gustavo Aragon <gustavo.aragon@fit.fraunhofer.de>
2018-03-29 17:34:10 +0200

    Controller of optimization daemon

--- optimization/controller.py
+++ optimization/controller.py
@@ -6,17 +6,49 @@ Created on Fri Mar 16 15:05:36 2018
 """
 
 from pyomo.environ import SolverFactory
-from optimization.ReferenceModel import model
-import os
+import optimization.models as models
+import os, logging
 
-def start():
-    project_dir=os.path.dirname(os.path.dirname(__file__))
-    data_file=project_dir+"/optimization/data.dat"
+logging.basicConfig(format='%(asctime)s %(levelname)s %(name)s: %(message)s', level=logging.DEBUG)
+logger = logging.getLogger(__file__)
 
-    instance = model.create_instance(data_file)
-    instance.pprint()
+class OptController:
 
-    opt=SolverFactory("glpk")
-    results=opt.solve(instance)
+    def __init__(self, object_name,solver_name,data_path, model_name):
+        logger.info("Initializing optimization controller")
 
-    print(results)
+
+        #Loading variables
+        self.name = object_name
+        self.results=""
+        self.model_name = model_name
+        self.data_path = data_path
+        self.solver_name = solver_name
+        #print("Problem solved with: " + solver_name)
+
+
+
+
+    """ 
+    Start the optimization process and gives back a result
+    """
+
+    def start(self):
+
+
+        logger.info("Starting optimization controller")
+        #Takimg the mathematical model from the configuration file
+
+        myModel="optimization.models.ReferenceModel"
+        from optimization.models.ReferenceModel import model
+
+        #Creating an optimization instance with the referenced model
+        instance = model.create_instance(self.data_path)
+        # instance.pprint()
+
+
+        opt = SolverFactory(self.solver_name)
+        self.results = opt.solve(instance)
+
+        #self.results = "hola"
+        return self.results
