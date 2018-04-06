28b201aa178708f2a0d18b15842a4602c2142f48
Gustavo Aragon <gustavo.aragon@fit.fraunhofer.de>
2018-04-06 17:12:12 +0200

    Optimization controller

--- optimization/controller.py
+++ optimization/controller.py
@@ -8,6 +8,8 @@ Created on Fri Mar 16 15:05:36 2018
 from pyomo.environ import SolverFactory
 import optimization.models as models
 import os, logging
+import importlib
+import importlib.util
 
 logging.basicConfig(format='%(asctime)s %(levelname)s %(name)s: %(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__file__)
@@ -26,8 +28,11 @@ class OptController:
         self.solver_name = solver_name
         #print("Problem solved with: " + solver_name)
 
-
-
+    # New way of importing
+    def path_import2(absolute_path):
+        spec = importlib.util.spec_from_file_location(absolute_path, absolute_path)
+        module = spec.loader.load_module(spec.name)
+        return module
 
     """ 
     Start the optimization process and gives back a result
@@ -39,7 +44,11 @@ class OptController:
         logger.info("Starting optimization controller")
         #Takimg the mathematical model from the configuration file
 
-        myModel="optimization.models.ReferenceModel"
+        try:
+            module = self.path_import2("/usr/src/app/optimization/models/model_1.py")
+        except Exception as e:
+            logger.error(e)
+
         from optimization.models.ReferenceModel import model
 
         #Creating an optimization instance with the referenced model
