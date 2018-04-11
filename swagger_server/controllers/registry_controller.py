0dc7ff3d11f9a06ae881d63c554e4510c7e7d0d9
Gustavo Aragon <gustavo.aragon@fit.fraunhofer.de>
2018-04-11 17:11:26 +0200

    controller for API /registry/

--- swagger_server/controllers/registry_controller.py
+++ swagger_server/controllers/registry_controller.py
@@ -1,37 +1,36 @@
 import connexion
 import six
 
-from swagger_server.models.datainput import Datainput  # noqa: E501
+from swagger_server.models.input_source import InputSource  # noqa: E501
+from swagger_server.models.output_source import OutputSource  # noqa: E501
 from swagger_server import util
 
 
-def change_input_channel_by_id(id, name=None):  # noqa: E501
-    """Change the parameters of a data source by id
-
-     # noqa: E501
-
-    :param id: Setting of the input channels for the optimization framework
-    :type id: int
-    :param name: Updated name of the channel
-    :type name: dict | bytes
-
-    :rtype: None
-    """
-    if connexion.request.is_json:
-        name = Datainput.from_dict(connexion.request.get_json())  # noqa: E501
-    return 'do some magic!'
-
-
-def set_input_channel(setInputSource):  # noqa: E501
+def load_source(InputSource):  # noqa: E501
     """Creates a new data source as input
 
      # noqa: E501
 
-    :param setInputSource: Data source to be created
-    :type setInputSource: dict | bytes
+    :param InputSource: Data source to be created
+    :type InputSource: dict | bytes
 
     :rtype: None
     """
     if connexion.request.is_json:
-        setInputSource = Datainput.from_dict(connexion.request.get_json())  # noqa: E501
+        InputSource = InputSource.from_dict(connexion.request.get_json())  # noqa: E501
+    return 'do some magic!'
+
+
+def output_source(OutputSource):  # noqa: E501
+    """Creates a new data source as ouput
+
+     # noqa: E501
+
+    :param OutputSource: Output data source to be created
+    :type OutputSource: dict | bytes
+
+    :rtype: None
+    """
+    if connexion.request.is_json:
+        OutputSource = OutputSource.from_dict(connexion.request.get_json())  # noqa: E501
     return 'do some magic!'
