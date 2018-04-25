a7b5fc9e52bb33212a1b48f0f1a8891ecbc0c088
Gustavo Aragon <gustavo.aragon@fit.fraunhofer.de>
2018-04-25 17:13:51 +0200

    API command controller

--- swagger_server/controllers/command_controller.py
+++ swagger_server/controllers/command_controller.py
@@ -1,30 +1,36 @@
 import connexion
 import six
 import logging
-import configparser
+
 
 from swagger_server.models.start import Start  # noqa: E501
 from swagger_server import util
 from flask import json
+from swagger_server.controllers.threadFactory import ThreadFactory
 
-from optparse import OptionParser
-from optimization.controller import OptController
+
 import os, time
 
 logging.basicConfig(format='%(asctime)s %(levelname)s %(name)s: %(message)s', level=logging.DEBUG)
 logger = logging.getLogger(__file__)
 
-def getFilePath(dir,file_name):
-    #print(os.path.sep)
-    #print(os.environ.get("HOME"))
-    project_dir = os.path.dirname(os.path.realpath(__file__))
-    data_file = os.path.join("/usr/src/app",dir,file_name)
-    return data_file
+
+class Object(object):
+
+    optimizationObject=None
+
+    def __init__(self):
+        self.isThereObject=False
+
+
+
 
 def getDataJSON(object, key):
     data=json.dumps(object)
     data2=json.loads(data)
     return data2[key]
+
+
 
 
 def framework_start(startOFW):  # noqa: E501
@@ -41,40 +47,17 @@ def framework_start(startOFW):  # noqa: E501
         logger.info("URL JSON received")
         startOFW = Start.from_dict(connexion.request.get_json())
 
-        try:
-            repetition = getDataJSON(startOFW,"repetition")
-            time_step= getDataJSON(startOFW,"time_step")
-            horizon = getDataJSON(startOFW, "horizon")
-            model_name = getDataJSON(startOFW,"model_name")
-            logger.info("Number of repetitions: "+str(repetition))
-            logger.info("Output with the following time steps: "+time_step)
-            logger.info("Optimization calculated with the following horizon: " + horizon)
-            logger.info("Optimization calculated with the following model: " + model_name)
 
-            # Creating an object of the configuration file
-            config = configparser.RawConfigParser()
-            config.read(getFilePath("utils","ConfigFile.properties"))
-            model_name = config.get("SolverSection", "model.name")
-            logger.info("This is the solver name: "+model_name)
-            model_path= os.path.join(config.get("SolverSection", "model.base.path"),model_name)+".py"
+        repetition = getDataJSON(startOFW,"repetition")
+        time_step= getDataJSON(startOFW,"time_step")
+        horizon = getDataJSON(startOFW, "horizon")
+        model_name = getDataJSON(startOFW,"model_name")
 
-            # Taking the data file name from the configuration file
-            data_file_name = config.get("SolverSection", "data.file")
-            data_path = getFilePath("optimization",data_file_name)
-
-            # Taking
-            solver_name = config.get("SolverSection", "solver.name")
-
-            opt = OptController("obj1", solver_name, data_path, model_path)
-
-            while True:
-                results = opt.start()
-                logger.info(results)
-                time.sleep(frequency)
-
-
-        except Exception as e:
-            logger.error(e)
+        factory = ThreadFactory(model_name, time_step, horizon, repetition)
+        Object.optimizationObject=factory
+        Object.isThereObject=True
+        logger.info("Thread: "+str(factory))
+        factory.startOptControllerThread()
     else:
         logger.error("Wrong Content-Type")
     return 'System started succesfully'
@@ -91,7 +74,14 @@ def framework_stop():  # noqa: E501
 
     try:
         logger.info("Stopping the system")
-        message = "System stopped succesfully"
+        if Object.isThereObject is True:
+            Object.optimizationObject.stopOptControllerThread()
+            Object.isThereObject = False
+            message = "System stopped succesfully"
+            logger.info(message)
+        else:
+            message="No threads found"
+            logger.info(message)
     except Exception as e:
         logger.error(e)
         message = "Error stoping the system"
