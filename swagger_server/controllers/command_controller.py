1532912993abfa5e643f0327bb623972eec2e6a5
Gustavo Aragon <gustavo.aragon@fit.fraunhofer.de>
2018-05-18 02:57:52 +0200

    controller of the command endpoint

--- swagger_server/controllers/command_controller.py
+++ swagger_server/controllers/command_controller.py
@@ -15,28 +15,55 @@ logging.basicConfig(format='%(asctime)s %(levelname)s %(name)s: %(message)s', le
 logger = logging.getLogger(__file__)
 
 
-class Object(object):
+class CommandController:
 
-    optimizationObject=None
+    factory=None
+    running=False
 
-    def __init__(self):
-        self.isThereObject=False
+    def set(self, object):
+        self.factory=object
 
+    def get(self):
+        return self.factory
 
+    def set_isRunning(self, bool):
+        self.running = bool
 
+    def get_isRunning(self):
+        return self.running
 
-def getDataJSON(object, key):
-    data=json.dumps(object)
-    data2=json.loads(data)
-    return data2[key]
+    def start(self, json_object):
+        self.model_name = json_object.model_name
+        self.time_step = json_object.time_step
+        self.horizon = json_object.horizon
+        self.repetition = json_object.repetition
+        #logger.info("Self.factory "+str(self.factory))
 
+        self.set(ThreadFactory(self.model_name, self.time_step, self.horizon, self.repetition))
 
+        #logger.info("Self.factory 2 " + str(self.factory))
+        logger.info("Thread: " + str(self.get()))
+        self.get().startOptControllerThread()
+        self.set_isRunning(True)
 
+    def stop(self):
+        logger.debug("Stop signal received")
+        logger.debug("This is the factory object: "+str(self.get()))
+        if self.factory:
+            self.factory.stopOptControllerThread()
+            self.set_isRunning(False)
+            message = "System stopped succesfully"
+            logger.debug(message)
+        else:
+            message="No threads found"
+            logger.debug(message)
+
+variable=CommandController()
 
 def framework_start(startOFW):  # noqa: E501
     """Command for starting the framework
 
-     # noqa: E501
+    # noqa: E501
 
     :param startOFW: Start command for the optimization framework
     :type startOFW: dict | bytes
@@ -44,20 +71,14 @@ def framework_start(startOFW):  # noqa: E501
     :rtype: None
     """
     if connexion.request.is_json:
-        logger.info("URL JSON received")
+        logger.info("Starting the system")
         startOFW = Start.from_dict(connexion.request.get_json())
 
+        if not variable.get_isRunning():
+            variable.start(startOFW)
+        else:
+            logger.debug("System already running")
 
-        repetition = getDataJSON(startOFW,"repetition")
-        time_step= getDataJSON(startOFW,"time_step")
-        horizon = getDataJSON(startOFW, "horizon")
-        model_name = getDataJSON(startOFW,"model_name")
-
-        factory = ThreadFactory(model_name, time_step, horizon, repetition)
-        Object.optimizationObject=factory
-        Object.isThereObject=True
-        logger.info("Thread: "+str(factory))
-        factory.startOptControllerThread()
     else:
         logger.error("Wrong Content-Type")
     return 'System started succesfully'
@@ -66,7 +87,7 @@ def framework_start(startOFW):  # noqa: E501
 def framework_stop():  # noqa: E501
     """Command for stoping the framework
 
-     # noqa: E501
+    # noqa: E501
 
 
     :rtype: None
@@ -74,15 +95,16 @@ def framework_stop():  # noqa: E501
 
     try:
         logger.info("Stopping the system")
-        if Object.isThereObject is True:
-            Object.optimizationObject.stopOptControllerThread()
-            Object.isThereObject = False
-            message = "System stopped succesfully"
-            logger.info(message)
+        if variable.get_isRunning():
+            logger.debug("System running and trying to stop")
+            variable.stop()
+            message="System stopped succesfully"
         else:
-            message="No threads found"
-            logger.info(message)
+            logger.debug("System already stopped")
+            message = "System already stopped"
+
     except Exception as e:
         logger.error(e)
         message = "Error stoping the system"
     return message
+
