f38d807112527273ea01b4d46e6f913f872a8894
Gustavo Aragon <gustavo.aragon@fit.fraunhofer.de>
2018-08-02 16:20:37 +0200

    Thread factory

--- swagger_server/controllers/threadFactory.py
+++ swagger_server/controllers/threadFactory.py
@@ -15,11 +15,12 @@ logger = logging.getLogger(__file__)
 
 class ThreadFactory:
 
-    def __init__(self, model_name, time_step, horizon, repetition):
+    def __init__(self, model_name, time_step, horizon, repetition, solver):
         self.model_name=model_name
         self.time_step=time_step
         self.horizon=horizon
         self.repetition=repetition
+        self.solver=solver
 
 
     def getFilePath(self,dir, file_name):
@@ -35,21 +36,28 @@ class ThreadFactory:
         logger.info("Output with the following time steps: " + str(self.time_step))
         logger.info("Optimization calculated with the following horizon: " + str(self.horizon))
         logger.info("Optimization calculated with the following model: " + self.model_name)
+        logger.info("Optimization calculated with the following solver: " + self.solver)
 
         # Creating an object of the configuration file
         config = configparser.RawConfigParser()
         config.read(self.getFilePath("utils", "ConfigFile.properties"))
-        self.model_name = config.get("SolverSection", "model.name")
-        logger.info("This is the model name: " + self.model_name)
-        self.model_path = os.path.join(config.get("SolverSection", "model.base.path"), self.model_name) + ".py"
+        if not self.model_name:
+            self.model_name = config.get("SolverSection", "model.name")
 
+        logger.debug("This is the model name: " + self.model_name)
+        self.model_path = os.path.join(config.get("SolverSection", "model.base.path"), self.model_name) + ".py"
+        logger.debug("This is the model name: " + str(self.model_path))
         # Taking the data file name from the configuration file
         data_file_name = config.get("SolverSection", "data.file")
         self.data_path = self.getFilePath("optimization", data_file_name)
 
         # Taking
-        self.solver_name = config.get("SolverSection", "solver.name")
+        if not self.solver:
+            self.solver_name = config.get("SolverSection", "solver.name")
+        else:
+            self.solver_name = self.solver
 
+        logger.debug("Optimization calculated with the following solver: " + self.solver_name)
         ##############################################################################################
         # Reads the registry/output and stores it into an object
         path = self.getFilePath("utils", "Output.registry")
@@ -75,7 +83,7 @@ class ThreadFactory:
         """Need to get data from config or input.registry?"""
         self.pv_forecast = True
         if "photovoltaic" in input_config.keys():
-            self.pv_forecast = bool(input_config["photovoltaic"]["Forecast"])
+            self.pv_forecast = bool(input_config["photovoltaic"]["Internal_Forecast"])
         if self.pv_forecast:
             pv_forecast_topic = config.get("IO", "pv.forecast.topic")
             pv_forecast_topic = json.loads(pv_forecast_topic)
@@ -86,7 +94,7 @@ class ThreadFactory:
         self.load_forecast = True
 
         if "load" in input_config.keys():
-            self.load_forecast = bool(input_config["load"]["Forecast"])
+            self.load_forecast = bool(input_config["load"]["Internal_Forecast"])
         if self.load_forecast:
             raw_data_topic = config.get("IO", "raw.data.topic")
             raw_data_topic = json.loads(raw_data_topic)
