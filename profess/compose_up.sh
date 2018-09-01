c8e73b7f7411b9f3156bad750c8814b46610a812
Gustavo Aragon <gustavo.aragon@fit.fraunhofer.de>
2018-09-01 23:48:21 +0200

    profess

--- profess/compose_up.sh
+++ profess/compose_up.sh
@@ -1,4 +1,4 @@
 echo "Building the Optimization Framework"
-sudo docker-compose -f docker-compose-arm.yml build ofw
+sudo docker-compose -f docker-compose-arm.yml build ofw mock
 echo "Starting the Optimization Framework"
-sudo docker-compose -f docker-compose-arm.yml up ofw
+sudo docker-compose -f docker-compose-arm.yml up ofw mock
