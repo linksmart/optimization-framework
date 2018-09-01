9c8472df914743d2f8a926ab9b4210ba5f5fd3e9
Gustavo Aragon <gustavo.aragon@fit.fraunhofer.de>
2018-09-01 23:46:44 +0200

    mock dat publisher

--- mock_data/mockSoCDataPublisher.py
+++ mock_data/mockSoCDataPublisher.py
@@ -20,12 +20,21 @@ logger = logging.getLogger(__file__)
 class MockSoCDataPublisher(DataPublisher):
 
     def __init__(self, topic_params, config):
-        super().__init__(False, topic_params, config, 5)
+        super().__init__(False, topic_params, config, 10)
         self.index = 20
+        self.value=20
 
     def get_data(self):
         meas = senml.SenMLMeasurement()
-        meas.name = "ESS_SoC_Value"
-        meas.value = random.randint(20,100)
+        meas.name = "SoC_Value"
+        #meas.value = random.randint(20,100)
+        self.value= self.value + 1
+        meas.value = self.value
         meas.time = time.time()
-        return json.dumps(meas.to_json())
+        logger.debug("meas: "+str(meas))
+        logger.debug("type meas: " + str(type(meas)))
+        val={"e":[json.loads(json.dumps(meas.to_json()))]}
+        logger.debug("Val: "+str(val))
+        val=json.dumps(val)
+        logger.debug("Sent MQTT:"+str(val))
+        return val
