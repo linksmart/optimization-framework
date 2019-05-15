5e90514418997cd21b0b70b70f22aa16964ddfbe
Nishit Gajjar <Nishit Gajjar>
2019-05-15 16:00:01 +0200

    connector changes

--- connector/Connector.py
+++ connector/Connector.py
@@ -19,8 +19,9 @@ class Connector(RecPub):
 
     def __init__(self, receiver_params, publisher_workers, config, house):
         self.pub_prefix = config.get("IO","pub.topic.prefix") + str(house) + "/"
-        self.key_level = int(config.get("KEY_META","level"))
-        self.key_separator = config.get("KEY_META","key.separator")
+        self.key_level = int(config.get(house,"key.level"))
+        self.key_separator = config.get(house,"key.separator", fallback="/")
+        self.data_type = config.get(house, "data.type", fallback="json")
         self.key_map = dict(config.items("KEYS"))
         self.house = house
         self.base = senml.SenMLMeasurement()
@@ -28,7 +29,10 @@ class Connector(RecPub):
         super().__init__(receiver_params, publisher_workers, config, house)
 
     def data_formater(self, data):
-        data = json.loads(data)
+        if self.data_type == "json":
+            data = json.loads(data)
+        elif self.data_type == "comma_equals":
+            data = self.comma_equals_to_dict(data)
         self.new_data = {}
         self.timestamp = int(time.time())
         if self.key_level == 1:
@@ -44,6 +48,29 @@ class Connector(RecPub):
                     self.timestamp = float(self.timestamp)
         self.level_traverse("", data, 0)
         return self.new_data.copy()
+
+    def comma_equals_to_dict(self, data):
+        new_data = {}
+        data = data.split(",")
+        for row in data:
+            if len(row) > 0:
+                if "=" in row:
+                    d = row.split("=")
+                    if len(d) == 2:
+                        k = d[0].strip()
+                        v = d[1].strip()
+                        if " " in k:
+                            k = k.split(" ")[-1]
+                        if " " in v:
+                            v = v.split(" ")[0]
+                        try:
+                            v = float(v)
+                            if v.is_integer():
+                                v = int(v)
+                        except Exception as e:
+                            pass
+                        new_data[k] = v
+        return new_data
 
     def level_traverse(self, key, data, level):
         if isinstance(data, dict):
