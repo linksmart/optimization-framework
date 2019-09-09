a8ee63e339a746cb7095f08528f5cdfc55d46aba
Gustavo Aragon <gustavo.aragon@fit.fraunhofer.de>
2019-09-09 13:40:04 +0200

    corrected frequency control

--- optimization/controllerDiscrete.py
+++ optimization/controllerDiscrete.py
@@ -29,6 +29,7 @@ class OptControllerDiscrete(ControllerBase):
 
     def optimize(self, action_handle_map, count, optsolver, solver_manager):
         while not self.redisDB.get_bool(self.stop_signal_key) and not self.stopRequest.isSet():
+            start_time_total = time.time()
             self.logger.info("waiting for data")
             data_dict = self.input.get_data(preprocess=False)  # blocking call
             self.logger.debug("Data is: " + json.dumps(data_dict, indent=4))
@@ -114,7 +115,10 @@ class OptControllerDiscrete(ControllerBase):
 
             self.logger.info("Optimization thread going to sleep for " + str(self.control_frequency) + " seconds")
             time_spent = IDStatusManager.update_count(self.repetition, self.id, self.redisDB)
-            for i in range(self.control_frequency - time_spent):
-                time.sleep(1)
-                if self.redisDB.get_bool(self.stop_signal_key) or self.stopRequest.isSet():
-                    break
+            final_time_total = time.time()
+            sleep_time = self.control_frequency - int(final_time_total-start_time_total)
+            if sleep_time > 0:
+                for i in range(sleep_time):
+                    time.sleep(1)
+                    if self.redisDB.get_bool(self.stop_signal_key) or self.stopRequest.isSet():
+                        break
