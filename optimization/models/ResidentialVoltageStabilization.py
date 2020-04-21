c6f72779d7554c339ba05ab1ceae854a3ecc3f11
garagonc <gustavo.aragon.c@gmail.com>
2020-04-21 18:21:34 +0200

    changed voltage model

--- optimization/models/ResidentialVoltageStabilization.py
+++ optimization/models/ResidentialVoltageStabilization.py
@@ -39,30 +39,40 @@ class Model:
 
     model.voltage_sensitivity_P = Param(within=Reals)  # voltage sensitivity factorof active power
 
+    model.P_Load_Input = Param(within=Reals)  # Active power demand
+    model.P_Grid_Input = Param(within=Reals)  # Active power demand
+
     ################################################################################################
 
     ##################################       VARIABLES             #################################
     ################################################################################################
 
     # grid
-    model.P_Grid_Output = Var(model.T, within=Reals)
+    model.P_Grid_Output = Var(model.T, within=Reals,bounds=(-model.P_Grid_Max_Export_Power, model.P_Grid_Max_Export_Power))
 
     # pv
     model.P_PV_Output = Var(model.T, within=NonNegativeReals, bounds=(0, model.PV_Inv_Max_Power))  # initialize=iniVal)
 
     # ess
-    model.P_ESS_Output = Var(model.T, within=Reals, bounds=(
-        -model.ESS_Max_Charge_Power, model.ESS_Max_Discharge_Power))  # ,initialize=iniSoC)
+    model.P_ESS_Output = Var(model.T, within=Reals,
+                             bounds=(-model.ESS_Max_Charge_Power, model.ESS_Max_Discharge_Power),
+                             initialize=0)  # ,initialize=iniSoC)
 
     model.SoC_ESS = Var(model.T_SoC, within=NonNegativeReals, bounds=(model.ESS_Min_SoC, model.ESS_Max_SoC))
 
     # voltage
     model.voltage_calculated = Var(model.T, within=NonNegativeReals)
 
+    model.P_ESS_Output_Pct = Var(model.T, within=Reals, initialize=0)
     model.P_Fronius = Var(model.T, within=Reals, bounds=(-model.Fronius_Max_Power, model.Fronius_Max_Power),
                           initialize=0)
     model.P_Fronius_Pct = Var(model.T, within=Reals, initialize=0)
     model.P_Fronius_Pct_Output = Var(model.T, within=Reals, initialize=0)
+
+    model.SoC_copy = Var(within=NonNegativeReals)
+    model.PV_copy = Var(within=NonNegativeReals)
+    model.Load_copy = Var(within=Reals)
+    model.Grid_copy = Var(within=Reals)
 
     ################################################################################################
 
@@ -71,22 +81,18 @@ class Model:
 
     # rule to limit the PV ouput to value of the PV forecast
     def con_rule_pv_potential(model, t):
-        return model.P_PV_Output[t] == model.P_PV[t]
-
-    # voltage
-    def con_rule_voltage(model, t):
-        return model.voltage_calculated[t] == (model.voltage_prediction[t] - model.voltage_sensitivity_P * (model.P_PV[t] - model.P_PV_Output[t]) + model.voltage_sensitivity_P * model.P_ESS_Output[t])
-
-    # rule for setting the maximum export power to the grid
-    def con_rule_grid_output_power(model, t):
-        return model.P_Grid_Output[t] >= -model.P_Grid_Max_Export_Power
-
+        return model.P_PV_Output[t] <= model.P_PV[t] / 1000
+    
     def con_rule_fronius_power(model, t):
         return model.P_PV_Output[t] + model.P_ESS_Output[t] == model.P_Fronius[t]
 
     # ESS SoC balance
     def con_rule_socBalance(model, t):
         return model.SoC_ESS[t + 1] == model.SoC_ESS[t] - model.P_ESS_Output[t] * model.dT / model.ESS_Capacity / 3600
+    
+    # voltage
+    def con_rule_voltage(model, t):
+        return model.voltage_calculated[t] == (model.voltage_prediction[t] - model.voltage_sensitivity_P * ((model.P_PV[t] / 1000) - model.P_PV_Output[t]) + model.voltage_sensitivity_P * model.P_ESS_Output[t])
 
     # initialization of the first SoC value to the value entered through the API
     def con_rule_iniSoC(model):
@@ -102,26 +108,47 @@ class Model:
 
     # Definition of the energy balance in the system
     def con_rule_energy_balance(model, t):
-        return model.P_Load[t] == model.P_Fronius[t] + model.P_Grid_Output[t]
+        return model.P_Load[t] / 1000 == model.P_Fronius[t] + model.P_Grid_Output[t]
 
     def con_rule_output_ess_power(model,t):
         return model.P_Fronius_Pct[t] == (100 / model.Fronius_Max_Power) * model.P_Fronius[t]
 
-    def con_rule_is_positive(model,t):
+    def con_rule_output_ess_power_pct(model, t):
+        return model.P_ESS_Output_Pct[t] == (100 / model.ESS_Max_Charge_Power) * model.P_ESS_Output[t]
+
+    def con_rule_is_positive(model, t):
         return model.P_Fronius_Pct[t] >= 0
 
     def con_rule_limiting_pct(model, t):
         return model.is_positive[t] * model.P_Fronius_Pct[t] == model.P_Fronius_Pct_Output[t]
 
-    model.con_pv_pmax = Constraint(model.T, rule=con_rule_pv_potential)
+    def con_rule_soc(model):
+        return model.SoC_copy == model.SoC_Value
+
+    def con_rule_pv(model):
+        return model.PV_copy == model.P_PV[0] / 1000
+
+    def con_rule_load(model):
+        return model.Load_copy == model.P_Load_Input / 1000
+
+    def con_rule_grid(model):
+        return model.Grid_copy == model.P_Grid_Input / 1000
+
+    model.con_pv_max = Constraint(model.T, rule=con_rule_pv_potential)
     model.con_voltage = Constraint(model.T, rule=con_rule_voltage)
-    model.con_grid_inv_P = Constraint(model.T, rule=con_rule_grid_output_power)
-    model.con_system_P = Constraint(model.T, rule=con_rule_energy_balance)
+    model.con_fronius_power = Constraint(model.T, rule=con_rule_fronius_power)
     model.con_ess_soc = Constraint(model.T, rule=con_rule_socBalance)
     model.con_ess_Inisoc = Constraint(rule=con_rule_iniSoC)
+    model.con_energy_balance = Constraint(model.T, rule=con_rule_energy_balance)
+    model.con_ess_percentage = Constraint(model.T, rule=con_rule_output_ess_power_pct)
     model.con_percentage = Constraint(model.T, rule=con_rule_output_ess_power)
     model.is_positive = Expression(model.T, rule=con_rule_is_positive)
     model.con_limiting_pct = Constraint(model.T, rule=con_rule_limiting_pct)
+
+    model.con_soc = Constraint(rule=con_rule_soc)
+    model.con_pv = Constraint(rule=con_rule_pv)
+    model.con_load = Constraint(rule=con_rule_load)
+    model.con_grid = Constraint(rule=con_rule_grid)
 
     ###########################################################################
     #######                         OBJECTIVE                           #######
